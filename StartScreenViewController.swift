f5902ca51cb2ab8d0c2f003f24e7512c34d6e442
aizaomar <w.omaraiza@gmail.com>
2021-09-12 02:01:19 +0600

    rename class

---
+++ StartScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartScreenViewController.swift
+//  SwiftQuiz
+//
+//  Created by Aiza on 11.09.2021.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
